996d3f10d84d41981dd8844cb812f302ac9b62c3
ohnozzy@gmail.com <ohnozzy@gmail.com@affda96c-c843-02ff-9111-937b6b35e041>
2012-11-07 08:58:02 +0000

    Add stub bool checkEdge and bool checkTri to lmath.cpp

--- Phase II/basics/lmath.h
+++ Phase II/basics/lmath.h
@@ -15,6 +15,7 @@ int signDet(LongInt& x1, LongInt& y1, LongInt& w1,
 int signDet(LongInt x[3][3]) ;
 
 int getside( array<LongInt, 3>& p2, array<LongInt, 3>& p3, array<LongInt, 3>& p);
+void getPlaneNorm( array<LongInt, 3>& p1, array<LongInt, 3>& p2, array<LongInt, 3>& p3, array<LongInt, 3>& norm);
 
 bool checkEdge(const LongInt& x1, const LongInt& y1, const LongInt& w1, const LongInt& x2, const LongInt& y2, const LongInt& w2, const LongInt& alpha);
 bool checkTri(const LongInt& x1, const LongInt& y1, const LongInt& w1, const LongInt& x2, const LongInt& y2, const LongInt& w2, const LongInt& x3, const LongInt& y3, const LongInt& w3, const LongInt& alpha);
